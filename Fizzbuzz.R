d335be726fa10c9375ad07432d3b0aae6f853ae7
Jhau16 <jenspeter-@hotmail.com>
2022-03-26 11:15:48 +0100

    Test connecting to github

---
+++ Fizzbuzz.R
@@ -0,0 +1,18 @@
+#FizzBuzz in R
+
+numbers <- 1:100
+
+for (x in numbers) {
+  if (x %% 15 == 0)
+    print("fizbuzz")
+  else if (x %% 5 == 0) {
+    print("buzz")
+  }
+  else if (x %% 3 == 0){
+    print("fizz")
+  }
+  else {
+    print(x)
+  }
+}
+  
